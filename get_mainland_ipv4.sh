0f6a188ab9e69026e0b84c0ba7b96f11cd0d02b6
coolpapa312 <papa312@gmail.com>
2024-11-17 20:07:44 +0800

    Add files via upload

---
+++ get_mainland_ipv4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /Volumes/Storage/Download
+wget -c http://ftp.apnic.net/stats/apnic/delegated-apnic-latest
+
+cat delegated-apnic-latest | awk -F '|' '/CN/&&/ipv4/ {print $4 "/" 32-log($5)/log(2)}' | cat > ipv4.txt
+cat > CN << EOF
+/log info "Import cn ipv4 cidr list..."
+/ip firewall address-list remove [/ip firewall address-list find list=CN]
+/ip firewall address-list
+EOF
+cat ipv4.txt | awk '{ printf(":do {add address=%s list=CN} on-error={}\n",$0) }' >> CN
+echo "}" >> CN
